50f84948af60cd9caf79503f3f6087d9ea05e929
electricessence <5899455+electricessence@users.noreply.github.com>
2025-05-20 23:22:56 -0700

    Enhance Spectre.Console logging integration with new options and formatter; add configuration support for themes, labels, and custom writers

--- Open.Logging.Extensions.SpectreConsole/LoggingBuilderExtensions.cs
+++ Open.Logging.Extensions.SpectreConsole/LoggingBuilderExtensions.cs
@@ -1,4 +1,7 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System;
 
 namespace Open.Logging.Extensions.SpectreConsole;
 
@@ -16,7 +19,72 @@ public static class LoggingBuilderExtensions
 	public static ILoggingBuilder AddSimpleSpectreConsole(
 		this ILoggingBuilder logging, string name = "simple-spectre-console-default")
 	{
+		if (logging == null)
+			throw new ArgumentNullException(nameof(logging));
+
 		var formatter = new SimpleSpectreConsoleFormatter();
-		return logging.AddSpecializedConsoleFormatter(name, formatter.WriteSynchronized);
+		
+		// Add the formatter directly
+		logging.AddConsole(options => options.FormatterName = name);
+		logging.Services.AddSingleton<ConsoleFormatter>(
+			new SpectreDelegateFormatter(name, formatter.WriteSynchronized));
+
+		return logging;
+	}
+
+	/// <summary>
+	/// Adds a Spectre.Console logger with the specified options.
+	/// </summary>
+	/// <param name="logging">The logging builder.</param>
+	/// <param name="configure">A delegate to configure the Spectre Console options.</param>
+	/// <param name="name">The formatter name.</param>
+	/// <returns>The logging builder with the Spectre.Console logger added.</returns>
+	public static ILoggingBuilder AddSpectreConsole(
+		this ILoggingBuilder logging,
+		Action<SpectreConsoleOptions> configure,
+		string name = "spectre-console-default")
+	{
+		if (logging == null)
+			throw new ArgumentNullException(nameof(logging));
+		if (configure == null)
+			throw new ArgumentNullException(nameof(configure));
+
+		var options = new SpectreConsoleOptions();
+		configure(options);
+
+		var formatter = new SimpleSpectreConsoleFormatter(
+			options.Theme,
+			options.Labels,
+			options.Writer);
+
+		// Add the formatter directly
+		logging.AddConsole(options => options.FormatterName = name);
+		logging.Services.AddSingleton<ConsoleFormatter>(
+			new SpectreDelegateFormatter(name, formatter.WriteSynchronized));
+
+		return logging;
+	}
+
+	/// <summary>
+	/// Adds a Spectre.Console logger with default options.
+	/// </summary>
+	/// <param name="logging">The logging builder.</param>
+	/// <param name="name">The formatter name.</param>
+	/// <returns>The logging builder with the Spectre.Console logger added.</returns>
+	public static ILoggingBuilder AddSpectreConsole(
+		this ILoggingBuilder logging,
+		string name = "spectre-console-default")
+	{
+		if (logging == null)
+			throw new ArgumentNullException(nameof(logging));
+
+		var formatter = new SimpleSpectreConsoleFormatter();
+		
+		// Add the formatter directly
+		logging.AddConsole(options => options.FormatterName = name);
+		logging.Services.AddSingleton<ConsoleFormatter>(
+			new SpectreDelegateFormatter(name, formatter.WriteSynchronized));
+
+		return logging;
 	}
 }
